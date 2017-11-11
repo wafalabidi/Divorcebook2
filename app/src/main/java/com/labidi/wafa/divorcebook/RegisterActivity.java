71694e08610d6b6d68cc2b1550cc4326017d1864
sofien <sofien.sahli@esprit.tn>
2017-11-12 00:07:14 +0100

    hzere

--- app/src/main/java/com/labidi/wafa/divorcebook/RegisterActivity.java
+++ app/src/main/java/com/labidi/wafa/divorcebook/RegisterActivity.java
@@ -1,11 +1,20 @@
 package com.labidi.wafa.divorcebook;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+
+import org.w3c.dom.Text;
 
 /**
  * Created by Wafa on 11/11/2017.
@@ -27,15 +36,14 @@ public class RegisterActivity extends BaseActivity implements View.OnClickListen
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         getSupportActionBar().hide();
-        getActionBar().hide();
         //Views
-        et_firstname = (EditText)findViewById(R.id.et_name);
-        et_lastname = (EditText)findViewById(R.id.et_lastname);
-        et_mail = (EditText)findViewById(R.id.et_mail);
-        et_pwd = (EditText)findViewById(R.id.et_pwd);
+        et_firstname = (EditText) findViewById(R.id.et_name);
+        et_lastname = (EditText) findViewById(R.id.et_lastname);
+        et_mail = (EditText) findViewById(R.id.et_mail);
+        et_pwd = (EditText) findViewById(R.id.et_pwd);
         //Buttons
-        btn_continue =(Button)findViewById(R.id.btn_register);
-
+        btn_continue = (Button) findViewById(R.id.btn_register);
+        btn_continue.setOnClickListener(this);
         // [START initialize_auth]
         mAuth = FirebaseAuth.getInstance();
         // [END initialize_auth]
@@ -44,6 +52,17 @@ public class RegisterActivity extends BaseActivity implements View.OnClickListen
 
     @Override
     public void onClick(View view) {
+        if (!TextUtils.isEmpty(et_mail.getText())&&(!TextUtils.isEmpty(et_pwd.getText()))) {
+            Log.e("CheeckPoint", "CheckPoint 1");
 
+            mAuth.createUserWithEmailAndPassword(et_mail.getText().toString(), et_pwd.getText().toString())
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            
+
+                        }
+                    });
+        }
     }
 }
